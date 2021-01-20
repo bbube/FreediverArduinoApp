177990ec560fad6916400dd84d64f2a51c4b878f
still-nameless <67115556+still-nameless@users.noreply.github.com>
2021-01-20 16:24:16 +0100

    Code cleaner, logfile problem solved

--- DiveComputer/src/main.cpp
+++ DiveComputer/src/main.cpp
@@ -7,13 +7,14 @@
 #include <iostream>
 #include <fstream>
 #include <Arduino_LSM6DS3.h>
+#include <ArduinoBLE.h>
 
 using namespace std;
 
 #define LOOP_STATE_STOPPED 0
 #define LOOP_STATE_STARTED 1
 
-bool directoryCreated = false;
+bool diveStarted = false;
 
 int resPin = 6;
 int mfioPin = 5;
@@ -24,24 +25,27 @@ int diveID = 1;
 float accX, accY, accZ;
 float gyrX, gyrY, gyrZ;
 
-String sessionPath = "session.txt";
-String divePath = "dive.txt";
+String sessionPath = "session.log";
+String divePath = "dive.log";
 String logPath;
 
 const int chipSelect = 4;
 
 SparkFun_Bio_Sensor_Hub bioHub(resPin, mfioPin);
-
 bioData body;
+
+BLEService DiveData("19B10000-E8F3-537E-4F6C-D194768A2214");
+BLEUnsignedCharCharacteristic DataStream("2101", BLERead | BLENotify);
 
 File dataFile;
 
-//<button>
-ezButton button(8);  // create ezButton object that attach to pin 7;
+ezButton btnDive(8);
+ezButton btnBluetooth(7);
 int loopState = LOOP_STATE_STOPPED;
-//</button>
 
 
+//returns the sessionID from the sessionID-file
+//returns -1 if failed to open the file
 int getSessionID() {
   File file = SD.open(sessionPath);
   if(file) {
@@ -52,6 +56,8 @@ int getSessionID() {
   return -1;
 }
 
+//returns the diveID from the diveID-file
+//returns -1 if failed to open the file
 int getDiveID() {
   File file = SD.open(divePath);
   if(file) {
@@ -62,6 +68,7 @@ int getDiveID() {
   return -1;
 }
 
+//writes the value of variable sessionID (+ parameter if given) into the sessionID-file
 void setSessionID(int x = 0) {
   File file = SD.open(sessionPath, FILE_WRITE);
   if(file) {
@@ -70,6 +77,7 @@ void setSessionID(int x = 0) {
   }
 }
 
+//writes the value of variable sessionID (+ parameter if given) into the sessionID-file
 void setDiveID(int x = 0) {
   File file = SD.open(divePath, FILE_WRITE);
   if(file) {
@@ -78,18 +86,15 @@ void setDiveID(int x = 0) {
   }
 }
 
+//creates a directory with given parameter as name
 void createDirectory(int x) {
   SD.mkdir(to_string(x).c_str());
   Serial.println("directory created");
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  button.setDebounceTime(50);
-
-  // ---------- Hearbeat etc -----------
-  Wire.begin();
+//-------biohub sensor--------
+//initializes the biohub-sensor
+void initializeBioHubSensor() {
   int result = bioHub.begin();
   if (result == 0) // Zero errors!
     Serial.println("Sensor started!");
@@ -106,14 +111,11 @@ void setup() {
     Serial.print("Error: "); 
     Serial.println(error); 
   }
+}
 
-  // Data lags a bit behind the sensor, if you're finger is on the sensor when
-  // it's being configured this delay will give some time for the data to catch
-  // up. 
-  Serial.println("Loading up the buffer with data");
-  //delay(4000);
-
-  //Initialize IMU for gyroscope and accellerator
+//-------IMU--------
+//Initialize IMU for gyroscope and accellerator
+void initializeIMU() {
   if(!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
     while(1);
@@ -121,8 +123,11 @@ void setup() {
   else {
     Serial.println("IMU initialized");
   }
+}
 
-  //-------SD----------
+//-------SD card--------
+//initializes the SD card
+void initializeSD() {
   Serial.print("Initializing SD card...");
   // see if the card is present and can be initialized:
   SD.begin(chipSelect);
@@ -132,16 +137,12 @@ void setup() {
     while (1);
   }
   Serial.println(" card initialized.");
+}
 
-    
-    //File sessionFile = SD.open(sessionPath.c_str(),FILE_WRITE);
-    //sessionFile.println(to_string(420).c_str());
-    //sessionFile.close();
-
-  
-  //-------META files---------
-  //Check if the Meta-Files "sessionID" and "diveID" exist. IF yes their current
-  //values are saved in sessionID and diveID. IF not they are created.
+//-------META files--------
+//Check if the meta-Files "sessionID" and "diveID" exist. IF yes their current
+//values are saved in sessionID and diveID. IF not they are created.
+void initializeMetaFiles() {
   if (SD.exists(sessionPath)) {
     sessionID = getSessionID() + 1;
     SD.remove(sessionPath);
@@ -153,70 +154,107 @@ void setup() {
   }
   Serial.println("sessionID initialized");
 
-  
   if (SD.exists(divePath.c_str())) {
     SD.remove(divePath.c_str());
   }
   setDiveID();
   Serial.println("diveID initialized");
-  
+}
 
-  logPath = String("") + sessionID + "/" + getDiveID() + ".txt";
+//-------BLE--------
+//initializing the BLE unit
+void inizializeBLE() {
+  if (!BLE.begin()) 
+  {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("DiveComputerV2");
+  DiveData.addCharacteristic(DataStream);
+  BLE.addService(DiveData);
+  BLE.setAdvertisedService(DiveData);  
+  DataStream.broadcast();
+  BLE.advertise();
+  Serial.println("Bluetooth device active, waiting for connections...");
+}
+
+String getData() {
+  body = bioHub.readBpm();
+   
+  if(IMU.accelerationAvailable()) {
+    IMU.readAcceleration(accX, accY, accZ);
+  }
+  if(IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(gyrX, gyrY, gyrZ);
+  }
+
+  //building the data-string for the log file:
+  //time, heartrate, oxygen, confidence, depth, brightness, temperature, acceleration(x,y,z), gyroscope(x,y,z)
+  return String("") + millis() + "," + body.heartRate + ","
+                    + body.oxygen + "," + body.confidence + "," 
+                    + (random(0, 500)/10) + "," + random(0, 255)
+                    + "," + (random(0, 300)/10) + "," + accX  + "," + accY
+                    + "," + accZ + "," + gyrX + "," + gyrY + "," + gyrZ; 
+}
+
+void logData(String data) {
+  dataFile = SD.open(logPath, FILE_WRITE);
+  if(dataFile){
+      dataFile.println(data); //dataString.c_str());
+      dataFile.close();
+      Serial.println("data logged...");
+  }
+  else {
+    Serial.println("error opining logfile");
+  }
+}
+
+void btnDiveClickListener() {
+  btnDive.loop(); // must call the loop() function first
+  if (btnDive.isPressed() && btnDive.getStateRaw()==0) {
+    if (loopState == LOOP_STATE_STOPPED){
+        loopState = LOOP_STATE_STARTED;
+        createDirectory(sessionID);
+        logPath = String("") + sessionID + "/" + diveID + ".log";  
+      
+    }
+    else if(loopState == LOOP_STATE_STARTED){
+      loopState = LOOP_STATE_STOPPED;
+      diveStarted = false;
+      dataFile.close();
+      diveID++;
+    }
+  }
+}
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  btnDive.setDebounceTime(5);
+  btnBluetooth.setDebounceTime(20);
+  Wire.begin();
+
+  initializeBioHubSensor();
+  initializeIMU();
+  initializeSD();
+  initializeMetaFiles();
+  inizializeBLE();
+
+  logPath = String("") + sessionID + "/" + getDiveID() + ".log";
   Serial.println(logPath);
   
 }
 
 void loop() {
 
-  button.loop(); // MUST call the loop() function first
   String dataString = "";
 
-  if (button.isPressed()) {
-    if (loopState == LOOP_STATE_STOPPED){
-      loopState = LOOP_STATE_STARTED;
-      createDirectory(sessionID);
-      logPath = String("") + sessionID + "/" + diveID + ".txt";
-    }
-    else if(loopState == LOOP_STATE_STARTED){
-      loopState = LOOP_STATE_STOPPED;
-      dataFile.close();
-      diveID++;
-    }
-  }
+  btnDiveClickListener();
 
-  
-  
   if (loopState == LOOP_STATE_STARTED) {
-
-    body = bioHub.readBpm();
-   
-    if(IMU.accelerationAvailable()) {
-      IMU.readAcceleration(accX, accY, accZ);
-    }
-    if(IMU.gyroscopeAvailable()) {
-      IMU.readGyroscope(gyrX, gyrY, gyrZ);
-    }
-
-    
-
-    //building the data-string for the log file:
-    //time, heartrate, oxygen, confidence, depth, brightness, 
-    dataString = String("") + millis() + "," + body.heartRate + ","
-                  + body.oxygen + "," + body.confidence + "," 
-                  + (random(0, 500)/10) + "," + random(0, 255)
-                  + "," + (random(0, 300)/10) + "," + accX  + "," + accY
-                  + "," + accZ + "," + gyrX + "," + gyrY + "," + gyrZ; 
-    
-    dataFile = SD.open(logPath, FILE_WRITE);
-    if(dataFile){
-        dataFile.println(dataString); //dataString.c_str());
-        dataFile.close();
-        Serial.println("data logged...");
-    }
-    else {
-      Serial.println("error opining logfile");
-    }
-
+    logData(getData());
     delay(100);
   }
 
