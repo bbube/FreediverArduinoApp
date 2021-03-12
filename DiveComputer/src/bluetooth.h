2df48f986c64f966ad53c1b2e45c05ccdf0b647b
still-nameless <67115556+still-nameless@users.noreply.github.com>
2021-03-12 20:48:32 +0100

    bluetooth experiments

--- DiveComputer/src/bluetooth.h
+++ DiveComputer/src/bluetooth.h
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include <SD.h>
 #include <vector>
+#include <string>
 
 //#include <bits/stdc++.h> 
 using namespace std; 
@@ -11,7 +12,23 @@ int dataLength = 300;
 BLEService DiveData("19B10000-E8F3-537E-4F6C-D194768A2214");
 BLECharacteristic DataStream( "19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify,350,(1==1));
 
-String testData = "{\"ref_dive\":1,\"duration\":9438,\"heart_freq\":0,\"oxygen_saturation\":0,\"heart_var\":0,\"depth\":93,\"luminance\":78,\"water_temp\":6,\"accelerator_x\":0.030151,\"accelerator_y\":0.002563,\"accelerator_z\":1.012573,\"gyroscope_x\":1.037598,\"gyroscope_y\":-3.173828,\"gyroscope_z\":-1.647949}";
+BLEFloatCharacteristic accelerator_x("094a4ab5-d789-477e-8b3a-fd5fee1971f9", BLERead | BLENotify);
+BLEFloatCharacteristic accelerator_y("63e30608-53f1-48fb-a7f1-8363ed609a39", BLERead | BLENotify);
+BLEFloatCharacteristic accelerator_z("82dea7b6-2091-4a8a-a788-fcaf0fba84e4", BLERead | BLENotify);
+BLEFloatCharacteristic depth("70b263a4-faa9-4940-96a6-ce481cfd5803", BLERead | BLENotify);
+BLEFloatCharacteristic duration("21980bb5-887a-4a8f-9598-51bb19b41068", BLERead | BLENotify);
+BLEFloatCharacteristic gyroscope_x("b6cf7ab8-525d-4e27-86e9-c4e7fe32223c", BLERead | BLENotify);
+BLEFloatCharacteristic gyroscope_y("c4fa00ce-5af9-40b7-b960-1658a8074320", BLERead | BLENotify);
+BLEFloatCharacteristic gyroscope_z("5475ba53-bcbd-43de-b410-c06ed3a50142", BLERead | BLENotify);
+BLEIntCharacteristic   heart_freq("5fa8fde9-8717-45b8-8d7a-2b4da78b627a", BLERead | BLENotify);
+BLEIntCharacteristic   heart_var("af2dc2ff-e565-4183-ae8a-a5bc018967c3", BLERead | BLENotify);
+BLEIntCharacteristic   luminance("4433820f-7709-4881-ad18-adc063ddc0a1", BLERead | BLENotify); 
+BLEIntCharacteristic   oxygen_saturation("ab9db830-e5b5-4c12-8c9b-845386bf6dc8", BLERead | BLENotify);
+BLEIntCharacteristic   ref_dive("b8589826-9f39-445d-901e-699db1cad791", BLERead | BLENotify);
+BLEFloatCharacteristic water_temp("3473bdad-806f-419f-875a-0f2472e3fe53", BLERead | BLENotify);
+
+BLEDescriptor nameDescriptor("2187", "1");
+//String testData = "{\"ref_dive\":1,\"duration\":9438,\"heart_freq\":0,\"oxygen_saturation\":0,\"heart_var\":0,\"depth\":93,\"luminance\":78,\"water_temp\":6,\"accelerator_x\":0.030151,\"accelerator_y\":0.002563,\"accelerator_z\":1.012573,\"gyroscope_x\":1.037598,\"gyroscope_y\":-3.173828,\"gyroscope_z\":-1.647949}";
 //std::vector<String> sessionsArray;
 
 String directoryPath2 = "logFiles";
@@ -30,17 +47,90 @@ void inizializeBLE() {
     while (1);
   }
   BLE.setLocalName("DiveComputer");
+  /*
   DiveData.addCharacteristic(DataStream);
   BLE.addService(DiveData);
   BLE.setAdvertisedService(DiveData);  
   DataStream.broadcast();
+  */
+
+  accelerator_x.addDescriptor(nameDescriptor);
+
+  
+  
+
+  DiveData.addCharacteristic(accelerator_x);
+  DiveData.addCharacteristic(accelerator_y);
+  DiveData.addCharacteristic(accelerator_z);
+  DiveData.addCharacteristic(depth);
+  DiveData.addCharacteristic(duration);
+  DiveData.addCharacteristic(gyroscope_x);
+  DiveData.addCharacteristic(gyroscope_y);
+  DiveData.addCharacteristic(gyroscope_z);
+  DiveData.addCharacteristic(heart_freq);
+  DiveData.addCharacteristic(heart_var);
+  DiveData.addCharacteristic(luminance);
+  DiveData.addCharacteristic(oxygen_saturation);
+  DiveData.addCharacteristic(ref_dive);
+  DiveData.addCharacteristic(water_temp);
+  
+  BLE.addService(DiveData);
+  BLE.setAdvertisedService(DiveData);
+  accelerator_x.broadcast();
+  accelerator_y.broadcast();
+  accelerator_z.broadcast();
+  depth.broadcast();
+  duration.broadcast();
+  gyroscope_x.broadcast();
+  gyroscope_y.broadcast();
+  gyroscope_z.broadcast();
+  heart_freq.broadcast();
+  heart_var.broadcast();
+  luminance.broadcast();
+  oxygen_saturation.broadcast();
+  ref_dive.broadcast();
+  water_temp.broadcast();
+  
   BLE.advertise();  
   Serial.println("Bluetooth device active, waiting for connections...");
 }
 
-void sendData(String data) {
-  //TODO
-  DataStream.writeValue(data.c_str());
+void sendData(
+  float _accelerator_x, 
+  float _accelerator_y, 
+  float _accelerator_z,
+  float _depth,
+  float _duration,
+  float _gyroscope_x,
+  float _gyroscope_y,
+  float _gyroscope_z,
+  int _heart_freq, 
+  int _heart_var, 
+  int _luminance,
+  int _oxygen_saturation,
+  int _ref_dive,
+  float _water_temp) {
+
+	accelerator_x.writeValue(_accelerator_x);
+  delay(10);
+	accelerator_y.writeValue(_accelerator_y);
+  delay(10);
+	accelerator_z.writeValue(_accelerator_z);
+  delay(10);
+	depth.writeValue(_depth);
+  delay(10);
+	duration.writeValue(_duration);
+  delay(10);
+	gyroscope_x.writeValue(_gyroscope_x);
+  delay(10);
+	gyroscope_y.writeValue(_gyroscope_y);
+	gyroscope_z.writeValue(_gyroscope_z);
+	heart_freq.writeValue(_heart_freq);
+	heart_var.writeValue(_heart_var);
+	luminance.writeValue(_luminance);
+	oxygen_saturation.writeValue(_oxygen_saturation);
+	ref_dive.writeValue(_ref_dive);
+	water_temp.writeValue(_water_temp);
 }
 
 
@@ -89,22 +179,35 @@ String getLastLineAndDeleteLine(String filename)
 void buildBluetoothConnectionTesting() {
 
   BLEDevice central = BLE.central();
+  float _accelerator_x = 0.04f;
+  float _accelerator_y = 0.02f;
+  float _accelerator_z = 100.83f;
+  float _depth = 34.92f;
+  float _duration = 232023.f;
+  float _gyroscope_x = 0.324f;
+  float _gyroscope_y = 0.367f;
+  float _gyroscope_z = 0.546f;
+  int _heart_freq = 183;
+  int _heart_var = 42;
+  int _luminance = 120;
+  int _oxygen_saturation = 98;
+  int _ref_dive = 4;
+  float _water_temp = 23.75;
 
   if(central) {
     Serial.print("Connected to central: ");
     Serial.println(central.address());
 
     while(central.connected()) {
-      
-      sendData(testData.c_str());
-      delay(200);
-
+		  sendData(_accelerator_x, _accelerator_y, _accelerator_z, _depth, _duration, _gyroscope_x, _gyroscope_y, _gyroscope_z, _heart_freq, _heart_var, _luminance, _oxygen_saturation, _ref_dive, _water_temp);
+		  delay(200);
     }
   }
   Serial.print("Disconnected from central: ");
   Serial.println(central.address());
 }
 
+/*
 void buildBluetoothConnection() {
 
   BLEDevice central = BLE.central();  
@@ -140,7 +243,7 @@ void buildBluetoothConnection() {
               Serial.println("inside while");
               data = file.readStringUntil('\n');
               Serial.print(data);
-              //sendData(data);
+              sendData(data);
             }
           }
           
@@ -151,6 +254,7 @@ void buildBluetoothConnection() {
         
         x++;
     } 
+    
       
     
   }
@@ -158,3 +262,4 @@ void buildBluetoothConnection() {
   Serial.print("Disconnected from central: ");
   Serial.println(central.address());
 }
+*/
